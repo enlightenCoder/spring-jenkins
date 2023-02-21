9d166758a4c71c566cb24de9ca92e04f60cf3b18
Ami Christopher, INI-DOS-OCD (EXT) <Christopher.Ami@swisscom.com>
2023-02-21 20:06:05 +0400

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoApplicationTest {
+
+    public static Logger logger = LoggerFactory.getLogger(DemoApplicationTest.class);
+    @Test
+    public void contextLoads() {
+        logger.info("Test case executed");
+        assertEquals(true, true);
+    }
+
+}
